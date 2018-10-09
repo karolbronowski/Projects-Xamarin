4cc3362acb2a7198a21961d1f54f80270e09f2be
Karol <karol.bronowski@gmail.com>
2018-10-10 00:19:22 +0200

    no message

---
+++ Defuse Bomb/Zaliczenie/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Zaliczenie
+{
+    public partial class MainPage : ContentPage
+    {
+        static string bomb = new Random().Next(1, 4).ToString();
+        static int scores = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        async void ButtonClicked(object sender, EventArgs e)
+        {
+            Button button = sender as Button;
+
+            if (button.Text == bomb)
+            {
+                await DisplayAlert("💥 Bomba wybuchła! 💥", "Koniec gry", "Spróbuj ponownie");
+                bomb = new Random().Next(1, 4).ToString();
+                scores = 0;
+            }
+            else
+            {
+                scores += 1;
+                await DisplayAlert("💣 Masz szczęście! 💣", "Wynik: " + scores, "Graj dalej!");
+                bomb = new Random().Next(1, 4).ToString();
+            }
+        }
+    }
+}
